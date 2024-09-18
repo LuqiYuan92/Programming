4f1383b1299960798f72ac14c503d8da9aaee2cc
Luqi Yuan <yuanluqi@yuanluqideMacBook-Air.local>
2024-09-18 23:37:55 +0800

    algorithm

--- hashtable/3longestString.js
+++ hashtable/3longestString.js
@@ -11,12 +11,16 @@ var lengthOfLongestSubstring = function (s) {
   for (right = 0; right < s.length; right++) {
     char = s[right];
     if (char in seen && seen[char] >= left) {
+      //seen[char] >= left is to check if the char is within sliding window of the current substring
       //seen[char] < right is default cuz the loop iteration is in an increasing order
       left = seen[char] + 1;
     } else {
       length = Math.max(right - left + 1, length);
     }
     seen[char] = right;
+    console.log(s.slice(left, right + 1));
   }
   return length;
 };
+
+s = "tmmzuxt";
